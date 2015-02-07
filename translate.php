eaa88c61e3543e3a75ce21bff8ce13825a0f18f7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-02-07 22:47:20 +0100

    translate: save updated file

--- translate.php
+++ translate.php
@@ -3,7 +3,9 @@
 <?php
 call_hooks("init"); /* Initializes all modules, also lang module */
 
-$data = json_decode(file_get_contents("data/openstreetmap-tag-translations/tags/de.json", "r"), true);
+$file = "data/openstreetmap-tag-translations/tags/de.json";
+
+$data = json_decode(file_get_contents($file), true);
 $template_str = array(
   'message'     => array(
     'name'        => "Singular",
@@ -35,7 +37,27 @@ foreach($data as $k => $v) {
 }
 
 $form = new form('lang', $form_def);
-$form->set_data($data);
+if($form->is_complete()) {
+  $data = $form->save_data();
+
+  // remove null values
+  foreach($data as $k=>$v) {
+    foreach($v as $k1=>$v1) {
+      if($v1 === null)
+        unset($data[$k][$k1]);
+    }
+
+    if((sizeof($data[$k]) == 1) && (isset($data[$k]['message'])))
+      $data[$k] = $data[$k]['message'];
+    elseif(sizeof($data[$k]) == 0)
+      $data[$k] = null;
+  }
+
+  file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
+}
+
+if($form->is_empty())
+  $form->set_data($data);
 
 Header("Content-Type: text/html; charset=UTF-8");
 ?>
@@ -47,7 +69,10 @@ Header("Content-Type: text/html; charset=UTF-8");
   <?php print print_add_html_headers(); /* prints additional html headers */ ?>
 </head>
 <body>
+<form method='post' enctype='multipart/form-data'>
 <?php
 print $form->show();
 ?>
+<input type='submit' value='Save'/>
+</form>
 </body>
